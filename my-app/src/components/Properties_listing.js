0332e17db09b7e0b45fec18f9579d8c04f4ce87e
hamsinipaladugu <hamsinipaladugu@gmail.com>
2022-10-28 13:17:04 -0500

    CSS changes

--- my-app/src/components/Properties_listing.js
+++ my-app/src/components/Properties_listing.js
@@ -11,7 +11,6 @@ function LayoutComponent() {
         fetch('properties.json')
             .then(res => res.json())
             .then(data => {
-                console.log(data)
                 setProperty(data)
             }
             )
@@ -20,15 +19,17 @@ function LayoutComponent() {
 
     return (
         <div>
+            <br />
             <SearchBar
                 filterText={filterText}
                 onFilterTextChange={setFilterText} />
+            <br />
             <div class="container overflow-hidden">
-            <Properties 
-                filterText={filterText}
-                property={property} />
+                <Properties
+                    filterText={filterText}
+                    property={property} />
             </div>
-             
+
         </div>
 
     );
@@ -37,119 +38,78 @@ function LayoutComponent() {
 function SearchBar({ filterText, onFilterTextChange }) {
     return (
         <div class="d-flex justify-content-center ">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 list-group-horizontal ">
-                        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                            <div class="input-group">
-                                <input type="text" value = {filterText} class="form-control" placeholder="Filter..."
-                                    aria-label="Input group example" aria-describedby="btnGroupAddon" onChange={(e) => onFilterTextChange(e.target.value)} />
-                                <div class="input-group-text" id="btnGroupAddon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-search" viewBox="0 0 16 16">
-                                        <path
-                                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                                    </svg>
-                                </div>
-                            </div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 list-group-horizontal ">
+                <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                    <div class="input-group">
+                        <input type="text" value={filterText} class="form-control" placeholder="Search..."
+                            aria-label="Input group example" aria-describedby="btnGroupAddon" onChange={(e) => onFilterTextChange(e.target.value)} />
+                        <div class="input-group-text" id="btnGroupAddon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-search" viewBox="0 0 16 16">
+                                <path
+                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                            </svg>
                         </div>
-                    </ul>
+                    </div>
                 </div>
-     /*   <form>
-            <input type="text" value={filterText} placeholder="Search..." onChange={(e) => onFilterTextChange(e.target.value)} />
-            <br></br>
-        </form> */
+            </ul>
+        </div>
     )
 }
 
 function Properties({ property, filterText }) {
     var val = filterText;
-    var rows =[];
+    var rows = [];
     property.forEach((video) => {
         if (
-          video.place.toLowerCase().indexOf(
-            filterText.toLowerCase()
-          ) === -1
+            video.place.toLowerCase().indexOf(
+                filterText.toLowerCase()
+            ) === -1
         ) {
-          return;
+            return;
         }
         rows.push(video);
-      });
+    });
     return (
-    //    <div class="row gy-4">
+        //    <div class="row gy-4">
         <div class="card-deck">
             {
                 rows.map((p) => (
-                    <div class ="col-md-3 ">
-                    <div class="card" >
-                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img class="card-img-top d-block w-100" src={p.path} alt="Card image cap" />
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block w-100" src="public/property-1.1.png" alt="Second slide" />
+                    <div class="col-3">
+                        <div class="card" style={{ marginBottom: 5 + 'em', minHeight: 24 + 'em' }}>
+                            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img class="card-img-top d-block w-100" src={p.path} alt="Card image cap" />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src="property-1.1.png" alt="Second slide" />
+                                    </div>
                                 </div>
                             </div>
+                            <div class="card-body">
+                                <h6 class="card-title">{p.place} &nbsp;&nbsp;&nbsp; {p.ratings}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
+                                    <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
+                                </svg></h6>
+                                <p class="card-text">
+                                    <small class="text-muted">
+                                        {p.host}<br>
+                                        </br>
+                                        {p.night_fee}<br>
+                                        </br>
+                                        {p.amenities}
+                                    </small>
+                                </p>
+                                <p class="card-text"><small class="text-muted"><b>About:</b> {p.short_description} </small></p>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">{p.place}</h5>
-                            <p class="card-text">
-                                <small class="text-muted">
-                                {p.host}<br>
-                                </br>
-                                {p.night_fee}<br>
-                                </br>
-                                {p.amenities}
-                                </small>
-                            </p>
-                            <p class="card-text"><small class="text-muted"><b>About:</b> {p.short_description} </small></p>
-                        </div>
-                    </div>
                     </div>
                 )
                 )
             }
         </div>
-    //    </div>
     );
 }
-
-/*function Properties1({ property }) {
-    return (
-        /*var mod_videos = [];
-        if(val === null || val === ' ')
-        {
-          mod_videos = prod;
-        }
-        else
-        {
-          if(val2)
-          {
-            mod_videos = prod.filter(d => {return d.available}) 
-            mod_videos = mod_videos.filter(d => {return d.title.startsWith(val)})
-          }
-          else
-          {
-            mod_videos = prod.filter(d => {return d.title.startsWith(val)})
-          } 
-        } 
-        property.map((p) => {
-            console.log(p);
-            var src = p.path;
-            console.log(src);
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={p.path} />
-                <Card.Body>
-                    <Card.Title>{p.place}</Card.Title>
-                    <Card.Text>
-                        {p.short_description}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            console.log("hello");
-        }
-        )
-    );
-} */
 
 class Properties_listing extends Component {
     render() {
@@ -157,9 +117,8 @@ class Properties_listing extends Component {
             <div>
                 <LayoutComponent />
             </div>
-
         );
-    }
+    } s
 }
 
 export default Properties_listing;
