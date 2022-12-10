5095d2a6aac3198b29d6a05daf0293d247f8808d
Hamsini Paladugu <hamsinipaladugu@gmail.com>
2022-12-10 01:10:32 -0600

    changes for property listing page filtering

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,72 +5,63 @@ var methodOveride = require('method-override');
 
 var user_id = "#";
 
-console.log("Inside properties router");
-
 var monk = require('monk');
-var db = monk('127.0.0.1:27017/Airbnb', function(err, db){
-    if(err){
-       console.error("Db is not connected", err.message);
-    }
-    else{
-        console.log("DB is connected");
-    }
+var db = monk('127.0.0.1:27017/Airbnb', function (err, db) {
+	if (err) {
+		console.error("Db is not connected", err.message);
+	}
+	else {
+		console.log("DB is connected");
+	}
 });
 
 
-router.get('/', function(req, res) {
-	console.log("inside this");
+router.get('/', function (req, res) {
 	var collection = db.get('properties');
-	collection.find({}, function(err, props){
+	collection.find({}, function (err, props) {
 		if (err) throw err;
 		res.json(props);
 	});
 });
 
-router.get('/properties/new', function(req, res) {
+router.get('/properties/new', function (req, res) {
 	res.render('new.ejs');
 
 });
 
-router.get('/reservations/newuser', function(req, res) {
+router.get('/reservations/newuser', function (req, res) {
 	res.render('newuser.ejs');
 });
 
-router.get('/reservations', function(req, res) {
-	console.log("inside this");
-	var t=new URLSearchParams(req.query);
+router.get('/reservations', function (req, res) {
+	var t = new URLSearchParams(req.query);
 	console.log(t);
-	if(t==0)
-	{
+	if (t == 0) {
 		console.log("No query value");
 		console.log(req.query);
 		var collection = db.get('reservations');
-	collection.find({}, function(err, props){
-		//res.json(videos);
-		if (err) throw err;
-	  	res.render('indexuser.ejs',{props : props})
-	});
-	}
-	else{
-		console.log("yes");
-		console.log(typeof(String(req.query.user_id)));
-		var collection = db.get('reservations');
-		collection.find({ user_id: String(req.query.user_id)}, function(err, result){
-			//console.log(user_id);
+		collection.find({}, function (err, props) {
+			//res.json(videos);
 			if (err) throw err;
-			//console.log(result);
-			  res.render('showuserid.ejs', { users : result});
-			//res.json(result);
+			res.render('indexuser.ejs', { props: props })
 		});
-	} 
-	
+	}
+	else {
+		console.log("yes");
+		console.log(typeof (String(req.query.user_id)));
+		var collection = db.get('reservations');
+		collection.find({ user_id: String(req.query.user_id) }, function (err, result) {
+			if (err) throw err;
+			res.render('showuserid.ejs', { users: result });
+		});
+	}
+
 });
 
-router.post('/reservations', function(req, res) {
-	//req.body is used to read form input
+router.post('/reservations', function (req, res) {
 	console.log(req.body);
 	var collection = db.get('reservations');
-	collection.insert({ 
+	collection.insert({
 		check_in: req.body.check_in,
 		check_out: req.body.check_out,
 		no_of_days: req.body.no_of_days,
@@ -79,166 +70,151 @@ router.post('/reservations', function(req, res) {
 		property_id: req.body.property_id,
 		user_id: req.body.user_id,
 		payment_id: req.body.payment_id
-	}, function(err, pr){
+	}, function (err, pr) {
 		if (err) throw err;
-		// if insert is successfull, it will return newly inserted object
-	  	//res.json(video);
 		res.redirect('/reservations');
 	});
 });
 
-router.delete('/reservations/:id', function(req, res) {
+router.delete('/reservations/:id', function (req, res) {
 	var collection = db.get('reservations');
-	collection.remove({ _id: req.params.id }, function(err, result){
+	collection.remove({ _id: req.params.id }, function (err, result) {
 		if (err) throw err;
 		res.redirect('/reservations');
-	  	//res.render('show', { pr : result[0] });
-		//res.json(result);
 	});
 });
 
-router.get('/reservations/:id', function(req, res) {
+router.get('/reservations/:id', function (req, res) {
 	var collection = db.get('reservations');
-	collection.find({ _id: req.params.id }, function(err, result){
+	collection.find({ _id: req.params.id }, function (err, result) {
 		if (err) throw err;
 		console.log(result);
-	  	res.render('showuser.ejs', { user : result[0]});
-		//res.json(result);
+		res.render('showuser.ejs', { user: result[0] });
 	});
 });
 
-  router.get('/new', function(req, res) {
-      res.render('new.ejs');
-  });
+router.get('/new', function (req, res) {
+	res.render('new.ejs');
+});
 
-  router.get('/properties', function(req, res) {
-	console.log("inside this property");
-	console.log(req.href);
-	console.log(user_id);
-	t=new URLSearchParams(req.query);
+router.get('/properties', function (req, res) {
+	t = new URLSearchParams(req.query);
 	console.log(t);
 	var collection = db.get('properties');
-	if(t==0 && user_id === "#")
-	{
+	if (t == 0 && user_id === "#") {
 		console.log("No query value");
 		console.log(req.query);
-		collection.find({}, function(err, props){
-		if (err) throw err;
-	  		res.render('index.ejs',{ props : props})
-	});
-	}
-	else{
-		if(t == 0)
-		{
-			collection.find({ host_id: user_id}, function(err, result){
+		collection.find({}, function (err, props) {
 			if (err) throw err;
-			res.render('index.ejs',{ props : result});
+			res.render('index.ejs', { props: props })
 		});
+	}
+	else {
+		if (t == 0) {
+			collection.find({ host_id: user_id }, function (err, result) {
+				if (err) throw err;
+				res.render('index.ejs', { props: result });
+			});
 		}
-		else
-		{
-			console.log(typeof(String(req.query.user_id)));
+		else {
+			console.log(typeof (String(req.query.user_id)));
 			user_id = String(req.query.user_id);
 			console.log("Inside else");
 			console.log(user_id);
-			collection.find({ host_id: String(req.query.user_id)}, function(err, result){
-			if (err) throw err;
-			res.render('index.ejs',{ props : result});
-		});
+			collection.find({ host_id: String(req.query.user_id) }, function (err, result) {
+				if (err) throw err;
+				res.render('index.ejs', { props: result });
+			});
 		}
-		
-	} 
-	
+
+	}
+
 });
-  
-router.post('/properties', function(req, res) {
-	//req.body is used to read form input
+
+router.post('/properties', function (req, res) {
 	var collection = db.get('properties');
-	collection.insert({ 
-		place: req.body.title,
+	collection.insert({
+		name: req.body.name,
+		city_name: req.body.city_name,
 		host: req.body.host,
 		path: req.body.image,
+		path2: req.body.image,
 		amenities: req.body.amenities,
 		ratings: req.body.ratings,
 		night_fee: req.body.night_fee,
 		service_fee: req.body.service_fee,
 		cleaning_fee: req.body.cleaning_fee,
 		short_description: req.body.short_description,
-		description:req.body.description,
-		host_id : req.body.host_id
-	}, function(err, pr){
+		description: req.body.description,
+		host_id: req.body.host_id,
+		bedrooms: req.body.bedrooms
+	}, function (err, pr) {
 		if (err) throw err;
 		res.redirect('/properties');
 	});
 });
 
-router.post('/properties/edit/:id', function(req, res) {
+router.post('/properties/edit/:id', function (req, res) {
 	var collection = db.get('properties');
 	var new_record = {
-		place: req.body.title,
+		name: req.body.name,
+		city_name: req.body.city_name,
 		host: req.body.host,
 		path: req.body.image,
+		path2: req.body.image,
 		amenities: req.body.amenities,
 		ratings: req.body.ratings,
 		night_fee: req.body.night_fee,
 		service_fee: req.body.service_fee,
 		cleaning_fee: req.body.cleaning_fee,
 		short_description: req.body.short_description,
-		description:req.body.description,
-		host_id : req.body.host_id
+		description: req.body.description,
+		host_id: req.body.host_id,
+		bedrooms: req.body.bedrooms
 	};
 
-	collection.update({ _id:req.params.id }, {$set : new_record}, {upsert:true}, function(err, pr){
+	collection.update({ _id: req.params.id }, { $set: new_record }, { upsert: true }, function (err, pr) {
 		if (err) throw err;
 		res.redirect('/properties');
 	});
 });
 
-router.delete('/properties/:id', function(req, res) {
+router.delete('/properties/:id', function (req, res) {
 	var collection = db.get('properties');
-	console.log("Property deleted");
-	collection.remove({ _id: req.params.id }, function(err, result){
+	collection.remove({ _id: req.params.id }, function (err, result) {
 		if (err) throw err;
-		//res.redirect('/properties');
-	  	res.render('show.ejs', { pr : result[0] });
-		//res.json(result);
+		res.render('show.ejs', { pr: result[0] });
 	});
 });
 
-router.get('/properties/:id/edit', function(req, res) {
+router.get('/properties/:id/edit', function (req, res) {
 	var collection = db.get('properties');
-	console.log("Inside edit function");
-	collection.find({ _id: req.params.id }, function(err, result){
+	collection.find({ _id: req.params.id }, function (err, result) {
 		if (err) throw err;
-	  	res.render('edit.ejs', { pr : result[0] });
-		//res.json(result);
+		res.render('edit.ejs', { pr: result[0] });
 	});
 });
 
-router.get('/properties/:id', function(req, res) {
+router.get('/properties/:id', function (req, res) {
 	var collection = db.get('properties');
-	collection.find({ _id: req.params.id }, function(err, result){
+	collection.find({ _id: req.params.id }, function (err, result) {
 		if (err) throw err;
-	  	res.render('show.ejs', { pr : result[0] });
-		//res.json(result);
 	});
 });
 
 router.get('/cart', function (req, res) {
-	console.log( "1");
 	var t = new URLSearchParams(req.query);
 	console.log(req.query.user_id);
-		try{
-			var collection = db.get('cart');
-		}
-		catch{
-			console.log("NO");
-		}
-		collection.find({ User_id: String(req.query.user_id) }, function (err, result) {
-			if (err) throw err;
-			//res.json(result);
-			res.render('cartuseids.ejs', { carts: result });
-		});
+	try {
+		var collection = db.get('cart');
+	}
+	catch {
+		console.log("NO");
+	}
+	collection.find({ User_id: String(req.query.user_id) }, function (err, result) {
+		if (err) throw err;
+		res.render('cartuseids.ejs', { carts: result });
+	});
 	// }
 });
 
@@ -246,40 +222,33 @@ router.get('/cart/:id', function (req, res) {
 	var collection = db.get('cart');
 	collection.find({ _id: req.params.id }, function (err, result) {
 		if (err) throw err;
-		// res.json(result);
 		res.render('cart.ejs', { cart: result[0] });
 	});
 });
 
 router.post('/cart/edit/:id', function (req, res) {
-	//req.body is used to read form input
 	var collection = db.get('cart');
 	console.log("POST")
 	console.log(req.body);
 	var new_record = {
-		No_of_days:req.body.No_of_days,
-		Total_Cost:req.body.Total_Cost,
-		Number_of_persons:req.body.number_of_persons,
-		Property_id:req.body.property_id,
-		User_id:req.body.User_id		
+		No_of_days: req.body.No_of_days,
+		Total_Cost: req.body.Total_Cost,
+		Number_of_persons: req.body.number_of_persons,
+		Property_id: req.body.property_id,
+		User_id: req.body.User_id
 	};
-	//console.log(new_record);
 	collection.update({ _id: req.params.id }, { $set: new_record }, { upsert: true }, function (err, pr) {
 		if (err) throw err;
-		// if insert is successfull, it will return newly inserted object
-		//res.json(video);
 		res.redirect('/properties');
 	});
-	
+
 });
 
 router.get('/cart/:id/edit', function (req, res) {
 	var collection = db.get('cart');
-	//console.log(req.params.id );
 	collection.find({ _id: req.params.id }, function (err, result) {
 		if (err) throw err;
 		res.render('editcart.ejs', { cart: result[0] });
-		//res.json(result);
 	});
 });
 
@@ -288,8 +257,7 @@ router.delete('/cart/:id', function (req, res) {
 	collection.remove({ _id: req.params.id }, function (err, result) {
 		if (err) throw err;
 		res.redirect('/properties');
-		//res.render('show', { pr : result[0] });
-		//res.json(result);
+
 	});
 });
 
--- my-app/src/components/Properties_listing.js
+++ my-app/src/components/Properties_listing.js
@@ -63,17 +63,17 @@ function Slide({ property, linkClicked }) {
     var rows = [];
 
     property.forEach((video) => {
-        if (video.id === linkClicked) {
+        if (video._id === linkClicked) {
             rows.push(video);
         }
     });
     return (
         rows.map((p) => (
-            <div class="card" id={p.id} style={{ marginBottom: 5 + 'em', minHeight: 24 + 'em' }} >
+            <div class="card" id={p._id} style={{ marginBottom: 5 + 'em', minHeight: 24 + 'em' }} >
                 <div id="carouselExampleSlides" class="carousel slide" data-ride="carousel" >
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img class="card-img-top d-block w-100" src={p.path} alt="First slide" />
+                            <img class="card-img-top d-block w-100" id={p._id} src={p.path} alt="First slide" />
                         </div>
                         <div class="carousel-item">
                             <img class="d-block w-100" src={p.path2} alt="Second slide" />
@@ -89,7 +89,7 @@ function Slide({ property, linkClicked }) {
                     </div>
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title"><span class="start">{p.place}</span><span class="end">{p.ratings}</span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
+                    <h5 class="card-title"><span class="start">{p.name} - {p.city_name}</span><span class="end">{p.ratings}</span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
                         <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
                     </svg></h5>
                     <p class="card-text">
@@ -121,7 +121,7 @@ function SearchBar({ filterText, onFilterTextChange }) {
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 list-group-horizontal ">
                 <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
                     <div class="input-group">
-                        <input type="text" value={filterText} class="form-control" placeholder="Search..."
+                        <input type="text" value={filterText} class="form-control" placeholder="Search by City/Property..."
                             aria-label="Input group example" aria-describedby="btnGroupAddon" onChange={(e) => onFilterTextChange(e.target.value)} />
                         <div class="input-group-text" id="btnGroupAddon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -143,8 +143,9 @@ function Properties({ property, filterText, linkClicked, onlinkClicked }) {
     var rows = [];
     //  buttonClicked(false);
     property.forEach((video) => {
+        var searchText = video.name + " - " + video.city_name;
         if (
-            video.place.toLowerCase().indexOf(
+            searchText.toLowerCase().indexOf(
                 filterText.toLowerCase()
             ) === -1
         ) {
@@ -157,11 +158,11 @@ function Properties({ property, filterText, linkClicked, onlinkClicked }) {
             {
                 rows.map((p) => (
                     <div class="col-3">
-                        <div class="card" id={p.id} style={{ marginBottom: 5 + 'em', minHeight: 24 + 'em' }} >
+                        <div class="card" id={p._id} style={{ marginBottom: 5 + 'em', minHeight: 24 + 'em' }} >
                             <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" >
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img class="card-img-top d-block w-100" id={p.id} src={p.path} alt="First slide" onClick={(e) => onlinkClicked(e.target.id)} />
+                                        <img class="card-img-top d-block w-100" id={p._id} src={p.path} alt="First slide" onClick={(e) => onlinkClicked(e.target.id)} />
                                     </div>
                                     <div class="carousel-item">
                                         <img class="card-img-top d-block w-100" src={p.path2} alt="Second slide" />
@@ -169,11 +170,12 @@ function Properties({ property, filterText, linkClicked, onlinkClicked }) {
                                 </div>
                             </div>
                             <div class="card-body">
-                                <h6 class="card-title">{p.place} &nbsp;&nbsp;&nbsp; {p.ratings}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
-                                    <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
-                                </svg></h6>
+                                <h6 class="card-title">{p.name} - {p.city_name}</h6>
                                 <p class="card-text">
                                     <small class="text-muted">
+                                        <b>Ratings: &nbsp;&nbsp;&nbsp; {p.ratings}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
+                                            <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
+                                        </svg></b><br />
                                         {p.host}<br>
                                         </br>
                                         {p.night_fee}<br>
